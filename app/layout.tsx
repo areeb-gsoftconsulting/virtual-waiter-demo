ba0e32f1fa3db67b7efa92a70be85ba2936491ce
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2024-09-11 14:49:16 +0500

    feat: changed name

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Nav } from "@/components/Nav";
 import { cn } from "@/utils";
 
 export const metadata: Metadata = {
-  title: "Hume AI - EVI - Next.js Starter",
-  description: "A Next.js starter using Hume AI's Empathic Voice Interface",
+  title: "Factory Girl Virtual Waiter",
+  description: "Factory Girl Virtual Waiter",
 };
 
 export default function RootLayout({
